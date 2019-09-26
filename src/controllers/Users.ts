242f8fb7c08be43d94c0105947b0a8a044d92ab3
Abhay Kumar <abhay.kumar@qed42.com>
2019-09-26 17:38:29 +0530

    service to 4000

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,6 +1,8 @@
 import UserModel from '../models/UserModel';
 import * as express from 'express';
 import Auth from "../services/JwtToken";
+import STATUS from '../services/status';
+import CheckDistance from '../services/checkLocation';
 class UserController {
 
     public getAllUsers(req: express.Request, res: express.Response, next: express.NextFunction): void {
@@ -33,26 +35,66 @@ class UserController {
 						});
 						next(error);
 				});
-    }
-    public updateUser(req: express.Request, res: express.Response, next: express.NextFunction): void {
+		}
+
+
+    public async updateUser(req: express.Request, res: express.Response, next: express.NextFunction) {
 				console.log(' In update USer :', req.body);
 				let updatePayload: any = {};
-				let attendance = {status: "present"}
-				UserModel.update(
-					req.params,
-					{$push: {attendance: attendance}}
-					)
-        .then((update) => {
-            res.status(200).json({ success: true });
-        })
-        .catch((error: Error) => {
-            res.status(500).json({
-                error: error.message,
-                errorStack: error.stack
-            });
-            next(error);
-        });
-    }
+				try {
+					let isValid = CheckDistance.isValidDistance(req.body.geoLocation.lat, req.body.geoLocation.lng);
+					console.log('isValid  in controller:', isValid);
+				if (req.body.status  && req.body.geoLocation) {
+					if (req.body.status === 'present' && isValid ) {
+						const returnCurrentDate = () => {
+							let hrs = new Date().getHours();
+							let minutesPre = new Date().getMinutes() < 10 ? true : false;
+							let min = new Date().getMinutes();
+							let time = minutesPre  ? hrs + ":0" +min  : hrs + ":" + min;
+							return time;
+						}
+						let attendance = {
+							timestamp: new Date(),
+							monthNubmer:new Date().getMonth() + 1,
+							monthName: new Date().toLocaleDateString('en-US', { month: 'short',timeZone: 'UTC' }),
+							year:  new Date().getFullYear(),
+							date: new Date().getDate(),
+							dayNumber: new Date().getDay(),
+							dayName: new Date().toDateString().split(' ')[0],
+							time: returnCurrentDate(),
+							status: STATUS.PRESENT
+						}
+						UserModel.update(
+							req.params,
+							{$push: {attendance: attendance}}
+							)
+							.then((update) => {
+									res.status(200).json({ success: true });
+							})
+							.catch((error: Error) => {
+								res.status(500).json({
+										error: error.message,
+										errorStack: error.stack
+								});
+								next(error);
+						});
+
+					} else {
+						throw 'Bad Request';
+					}
+				} // main if
+				else {
+					throw 'Bad Request';
+				}
+			}
+			catch (err ) {
+				res.status(500).json({
+					error: err,
+				});
+			}
+
+		}
+
     public createUser(req: express.Request, res: express.Response, next: express.NextFunction): void {
 			UserModel
 				.findOne({
@@ -79,7 +121,6 @@ class UserController {
 						email: email
 					})
 					.then( async (data) => {
-						console.log('USER CREATED :', data);
 						// let jwtToken = await Auth.mobileTokenGenerate(req.body.key);
 						// res.status(200).json({ data: data, jwt: jwtToken });
 						res.status(200).json({ data: data});
