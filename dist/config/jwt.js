a845da2276dd80d5c05e7dfd987250005e221be1
Abhay Kumar <abhay.kumar@qed42.com>
2019-09-27 13:52:41 +0530

    updated gitignore

--- dist/config/jwt.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const secret = {
-    secretKey: 'My-Secret-Key',
-};
-const JWT_TOKEN = { secret };
-exports.default = JWT_TOKEN;
-//# sourceMappingURL=jwt.js.map
--- dist/services/status.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Status = Object.freeze({
-    PRESENT: "present",
-    WFH: "WFH"
-});
-exports.default = Status;
-//# sourceMappingURL=status.js.map
