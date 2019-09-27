04bda7df7476c96c9f76511cdd68744a124411c4
Abhay Kumar <abhay.kumar@qed42.com>
2019-09-27 17:28:16 +0530

    pointing to mlab

--- src/env/development.ts
+++ src/env/development.ts
@@ -2,7 +2,10 @@
 
 export const envConfig: any = {
     database: {
-        MONGODB_URI: 'mongodb://localhost:27017/',
-        MONGODB_DB_MAIN: 'qed42_attendance'
+        // MONGODB_URI: 'mongodb://localhost:27017/',
+        // MONGODB_URI: 'mongodb+srv://Abhay1415:Abhay1415@qed42-attendance-dofnt.mongodb.net/test?retryWrites=true&w=majority',
+        MONGODB_URI: 'mongodb://Abhay1415:Abhay1415@ds235711.mlab.com:35711/',
+        // MONGODB_URI: 'mongodb+srv://Abhay1415:Abhay1415@qed42-attendance-dofnt.mongodb.net/',
+        MONGODB_DB_MAIN: 'qed42-attendance'
     }
 };
